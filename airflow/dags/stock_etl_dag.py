f747d4c3bb3dc766eda56c66db1a334969bb3415
SANJEEVIKUMAR <126496788+SanjeevikumarWD@users.noreply.github.com>
2025-05-26 12:12:57 +0530

    Create stock_etl_dag.py

---
+++ airflow/dags/stock_etl_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    "stock_etl",
+    start_date=datetime(2025, 5, 26),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+    generate_data = BashOperator(
+        task_id="generate_data",
+        bash_command="python /app/scripts/generate_data.py"
+    )
+    run_etl = BashOperator(
+        task_id="run_etl",
+        bash_command="python /app/scripts/etl_pipeline.py"
+    )
+    run_dbt = BashOperator(
+        task_id="run_dbt",
+        bash_command="dbt run --project-dir /app/dbt"
+    )
+    generate_data >> run_etl >> run_dbt
